d844f4ec39a213e371d9b8ce5506c1f7a4fbdf05
Samuel <samuelteweldebrhan@gmail.com>
2024-09-15 19:57:45 -0400

    Initial commit
    fix all the bugs

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -16,6 +16,7 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        // Here the camera follows the plane 
         transform.position = plane.transform.position + offset;
     }
 }
